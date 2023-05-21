8a0167d00bb68c5dfad37680f5915ff467fc6077
IVRYN <aizofmulia@gmail.com>
2023-05-21 18:09:35 +0800

    Added validation app, but haven't added functionality

--- transcript_verification_system/transcript_verification_system/settings.py
+++ transcript_verification_system/transcript_verification_system/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'validate.apps.ValidateConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- transcript_verification_system/validate/models.py
+++ transcript_verification_system/validate/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Transcript(models.Model):
+    id          =   models.BigIntegerField(primary_key=True, editable=False, null=False)
+    student_id  =   models.BigIntegerField(null=False, editable=False)
+    signature   =   models.CharField(max_length=512)
