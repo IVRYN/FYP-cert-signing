e523bc31aa82ba5a290190efd5aac5d28e7cd6b4
IVRYN <aizofmulia@gmail.com>
2023-05-21 21:26:07 +0800

    Added working transcript and grade backend

--- transcript_verification_system/validate/admin.py
+++ transcript_verification_system/validate/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Transcript, Subject, SubjectInTranscript
+
 # Register your models here.
+admin.site.register(Transcript)
+admin.site.register(Subject)
+admin.site.register(SubjectInTranscript)
--- transcript_verification_system/validate/models.py
+++ transcript_verification_system/validate/models.py
@@ -1,7 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    id              =   models.CharField(primary_key=True, max_length=10, editable=True, default="ABC0000")
+    name            =   models.CharField(max_length=256, editable=True)
+
+    def __str__(self):
+        return f'{self.id} {self.name}'
+
 class Transcript(models.Model):
-    id          =   models.BigIntegerField(primary_key=True, editable=False, null=False)
-    student_id  =   models.BigIntegerField(null=False, editable=False)
-    signature   =   models.CharField(max_length=512)
+    id              =   models.BigAutoField(primary_key=True, editable=False, null=False)
+    student_id      =   models.BigIntegerField(null=False, editable=True)
+    subjects        =   models.ManyToManyField(Subject, through="SubjectInTranscript")
+    signature       =   models.CharField(max_length=512, editable=False)
+    date_created    =   models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.id
+
+class SubjectInTranscript(models.Model):
+    id              =   models.BigAutoField(primary_key=True)
+    transcript      =   models.ForeignKey("Transcript", on_delete=models.CASCADE)
+    subject         =   models.ForeignKey("Subject", on_delete=models.CASCADE)
+    marks           =   models.BigIntegerField(editable=True, null=False, default=0)
+    grade           =   models.CharField(editable=True, null=False, max_length=2, default="A")
+
+    def __str__(self):
+        return f'{self.subject}'
