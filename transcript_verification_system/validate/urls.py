1502969755be538a9524e82498ffb369ec7c0949
IVRYN <aizofmulia@gmail.com>
2023-05-29 17:32:05 +0800

    Added validation views and urls

--- transcript_verification_system/validate/urls.py
+++ transcript_verification_system/validate/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("validate", views.validate_transcript, name="validate_transcript"),
+    path("validate/<str:signature>", views.validate_status, name="validate_status"),
     path("<int:stud_id>/", views.get_transcript, name="transcript")
 ]
--- transcript_verification_system/validate/views.py
+++ transcript_verification_system/validate/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
-from .models import Transcript, SubjectInTranscript
-from .forms import GetTranscript
+from django.conf import settings
+
+import random
+import hashlib
+import os
+
+from .models import Transcript, SubjectInTranscript, Student
+from .forms import GetTranscript, ValidateTranscript
 
 # Create your views here.
 
@@ -35,3 +41,38 @@ def index(request):
         form = GetTranscript()
 
     return render(request, "validate/index.html", {"form" : form})
+
+def validate_transcript(request):
+    if request.method == "POST":
+        form = ValidateTranscript(request.POST, request.FILES)
+
+        if form.is_valid():
+            transcript_sig = file_handling(request.FILES['transcript'])
+
+            return redirect("validate_status", signature=transcript_sig)
+
+        return redirect("validate_transcript")
+    else:
+        form = ValidateTranscript()
+
+        return render(request, "validate/validation.html", {"form" : form})
+
+def validate_status(request, signature):
+    transcript  =   Transcript.objects.filter(signature=signature)
+    status      =   transcript.exists()
+
+    if status:
+        student =   Student.objects.get(id=transcript[0].student_id)
+    else:
+        student =   {'id' : 'Not Found', 'name' : 'Not Found'}
+
+    return render(request, "validate/status.html", {"signature" : signature,
+                                                    "transcript" : transcript,
+                                                    "student"   : student,
+                                                    "status" : status
+                                                    })
+
+# Handle the verification files
+def file_handling(file):
+    signature = hashlib.sha256(file.read()).hexdigest()
+    return signature
